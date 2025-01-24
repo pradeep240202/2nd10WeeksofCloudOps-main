fd8f4fee4e2a9299563a8e6f0f5c924cb66101ad
pradeep240202 <121566936+pradeep240202@users.noreply.github.com>
2025-01-24 15:53:58 +0530

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket = "devops-bkt-11"
     key    = "eks/terraform.tfstate"
-    region = "east-key-pair"
+    region = "us-east-1"
   }
 
   required_version = ">= 1.6.3"
